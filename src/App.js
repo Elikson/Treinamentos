80639cc4e259d024a072e9ddc9cf7f0fb5b0d917
Elikson Tavares <eliksonbt.eks@gmail.com>
2023-01-19 15:26:07 -0400

    fix: outra forma de redirecionar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import React from 'react'
 function App() {
 
   React.useEffect(() => {
-    window.location.replace('https://www.udemy.com/user/elikson-bastos-tavares');
-  }, [])
+    const timeout = setTimeout(() => {
+      // 👇️ redirects to an external URL
+      window.location.replace('https://www.udemy.com/user/elikson-bastos-tavares');
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
     <div>App</div>
