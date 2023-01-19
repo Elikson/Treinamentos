664891ee0ad180e79009dda5a98b2233f4f00b83
Elikson Tavares <eliksonbt.eks@gmail.com>
2023-01-19 15:41:09 -0400

    fix: removido redirecionamento

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import React from 'react'
 
 function App() {
 
-  React.useEffect(() => {
-    const timeout = setTimeout(() => {
-      // 👇️ redirects to an external URL
-      window.location.replace('https://www.udemy.com/user/elikson-bastos-tavares');
-    }, 500);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
-    <div>App</div>
+    <div>Funcionando</div>
   )
 }
 
