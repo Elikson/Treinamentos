526fc95a252955bda4be2b4eab6661c35b2efbbc
Elikson Tavares <eliksonbt.eks@gmail.com>
2023-01-19 14:40:49 -0400

    feat: adicionado redirecionamento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function App() {
+
+  React.useEffect(() => {
+    window.location.href = "https://www.udemy.com/user/elikson-bastos-tavares"
+  }, [])
+
   return (
     <div>App</div>
   )
